92e1af19119ee389ea5e41938491741ce21cd034
matherthal <matherthal@gmail.com>
2015-11-24 10:15:39 -0500

    correcting descriptions and comments

--- Non-Personalized Recommenders/Assignment1.R
+++ Non-Personalized Recommenders/Assignment1.R
@@ -1,27 +1,26 @@
-# There are 4 deliverables for this assignment. Each deliverable represents a different analysis of the data provided to you. For each deliverable, you will submit a list of the top 5 movies as ranked by a particular metric. The 4 metrics are:
-#   
-#1. Mean Rating: Calculate the mean rating for each movie, order with the highest rating listed first, and submit the top 5.
-# 
-#2. % of ratings 4+: Calculate the percentage of ratings for each movie that are 4 or higher. Order with the highest percentage first, and submit the top 5.
-# 
-#3. Rating Count: Count the number of ratings for each movie, order with the most number of ratings first, and submit the top 5.
-# 
-#4. Top 5 Star Wars: Calculate movies that most often occur with Star Wars: Episode IV - A New Hope (1977) using the (x+y)/x method described in class. In other words, for each movie, calculate the percentage of Star Wars raters who also rated that movie. Order with the highest percentage first, and submit the top 5.
+## There are 4 deliverables for this assignment. Each deliverable represents a different analysis of the data provided to you. For each deliverable, you will submit a list of the top 5 movies as ranked by a particular metric. 
 
-setwd('/home/matheus/Dropbox/workspace/Coursera/recommender-systems/')
+# Set path where is located the CSV file
+# setwd('...')
 
+# Read data into raw
 raw <- read.csv("A1Ratings.csv")
-#21 colunas
-#1a col é id user
+
+# Separate movie rates from id of users (first column)
 raw2 <- raw[, 2:21]
 
-#médias
+
+##1. Mean Rating: Calculate the mean rating for each movie, order with the highest rating listed first, and submit the top 5.
+
+# apply mean function over the columns
 means <- sapply(raw2, FUN = mean, na.rm=TRUE)
+# order result
 means <- means[order(means, decreasing = TRUE)]
-means[1:5]
+print(means[1:5])
 
 
-#segundo desafio % of ratings 4+
+##2. % of ratings 4+: Calculate the percentage of ratings for each movie that are 4 or higher. Order with the highest percentage first, and submit the top 5.
+
 # filter_ge4 <- raw2>=4
 # filter_ge4[is.na(filter_ge4)] <- FALSE
 # data_ge4 <- raw2
@@ -35,30 +34,38 @@ data_ge4 <- raw2
 data_ge4[!filter_ge4] <- NA
 count_ge4 <- apply(filter_ge4, MARGIN = 2, FUN = sum, na.rm=TRUE)
 
-#funciona para o exemplo
+# works for the example
 #x <- raw[,20]
 #sum(!is.na(x[x>=4])) / sum(!is.na(x))
-#generalizando:
+
 calcPercentGE4 <- function(x) {
   sum(!is.na(x[x>=4])) / sum(!is.na(x))
 }
 percentGE4 <- apply(raw2, MARGIN = 2, FUN = calcPercentGE4)
-#arrendondar para baixo
+# round down, as in the example
 percentGE4 <- floor(percentGE4 * 1000)/10
+# order result
 percentGE4 <- percentGE4[order(percentGE4, decreasing = TRUE)]
-percentGE4[1:5]
+print(percentGE4[1:5])
 
 
-#count ratings
+##3. Rating Count: Count the number of ratings for each movie, order with the most number of ratings first, and submit the top 5.
+
+# get dataset of TRUE and FALSE corresponding to the fact of if there is rating or not
 data_has_rate <- !is.na(raw2)
+# count number of ratings (TRUE is 1 and FALSE is 0)
 count_ratings <- apply(data_has_rate, MARGIN = 2, FUN=sum, na.rm=FALSE)
+# order result
 count_ratings <- count_ratings[order(count_ratings, decreasing = TRUE)]
-count_ratings[1:5]
+print(count_ratings[1:5])
+
+
+##4. Top 5 Star Wars: Calculate movies that most often occur with Star Wars: Episode IV - A New Hope (1977) using the (x+y)/x method described in class. In other words, for each movie, calculate the percentage of Star Wars raters who also rated that movie. Order with the highest percentage first, and submit the top 5.
 
 # starwars
 names(raw2)[1]
 
-#funciona para a coluna 19 do exemplo
+# works for column 19 from the example
 # x <- raw2[, c(1,19)]
 # sw_notNA <- !is.na(x[1]) 
 # col_notNA <- !is.na(x[2])
@@ -88,4 +95,4 @@ data_noSW <- raw2[, 2:ncol(raw2)]
 occurWithSW <- apply(data_noSW, MARGIN = 2, FUN = calcOccurWithSW)
 #ordering and cropping
 occurWithSW <- occurWithSW[order(occurWithSW, decreasing = TRUE)]
-occurWithSW[1:5]
+print(occurWithSW[1:5])
